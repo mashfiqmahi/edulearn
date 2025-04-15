700b87d91cbe70e1981c1c6ce8b632d34d917837
Mashfiq Mahi <kazimashfiqju@gmail.com>
2025-04-15 23:41:10 +0600

    CSRF

--- mysite/settings_rail.py
+++ mysite/settings_rail.py
@@ -2,6 +2,7 @@ from mysite.settings import *
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
+CSRF_TRUSTED_ORIGINS = ['https://web-production-5068.up.railway.app']
 
 DATABASE = {
     'ENGINE':'django.db.backends.postgresql_psycopg2',
