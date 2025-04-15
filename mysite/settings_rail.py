2246a69f314ec525cd8191bdc9c6e3ae3bc99a93
Mashfiq Mahi <kazimashfiqju@gmail.com>
2025-04-15 22:58:32 +0600

    postgre sql settings update

--- mysite/settings_rail.py
+++ mysite/settings_rail.py
@@ -2,3 +2,13 @@ from mysite.settings import *
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
+
+DATABASE = {
+    'ENGINE':'django.db.backends.postgresql_psycopg2',
+    'NAME': config('DATABASE_NAME'),
+    'USER': config('DATABASE_USER'),
+    'PASSWORD': config('DATABASE_PASSWORD'),
+    'HOST': config('DATABASE_HOST'),
+    'PORT': config('DATABASE_PORT'),
+    'OPTIONS': {'sslmode':'require'},
+}
